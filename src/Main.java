6e760cb558e289eb71d1811eb292e1299e40c4bc
SeokHun <tommya98@naver.com>
2024-08-10 13:04:18 +0900

    구구단 - 8,9단 구현 - 값 입력 및 조건문

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("6단");
-        int i = 1;
-        while (i < 10) {
-            System.out.println(6 * i);
-            i++;
-        }
+        System.out.println("구구단 중 출력할 단은? : ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-        System.out.println("7단");
-        for(i = 1; i < 10; i++) {
-            System.out.println(7 * i);
+        if (number < 2) {
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        } else if (number > 9) {
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        } else {
+            System.out.println("number = " + number);
+
+            for (int i = 1; i < 10; i++) {
+                System.out.println(number * i);
+            }
         }
     }
 }
