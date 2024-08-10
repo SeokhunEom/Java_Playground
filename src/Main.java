4a2bab8498b97763f32fda423a67f5e206048176
SeokHun <tommya98@naver.com>
2024-08-10 21:47:24 +0900

    캘린더 - 종료 조건을 이용해 반복하기

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] monthArray = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        System.out.println("반복횟수를 입력하세요.");
         Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
 
-        for (int i = 0; i < count; i++) {
+        while (true) {
             System.out.println("달을 입력하세요.");
+            System.out.print("> ");
+
             int month = scanner.nextInt();
+            if (month == -1) {
+                System.out.println("Have a nice day!");
+                break;
+            }
+            if (month > 12 || month < 1) {
+                System.out.println("1부터 12까지의 숫자를 입력해주세요.");
+                continue;
+            }
+
             System.out.println(month + "월은 " + monthArray[month - 1] + "일까지 있습니다.");
         }
-
         scanner.close();
     }
 }
