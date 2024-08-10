0f3f6fe040cadfb5631ca5b34410ef3138efb542
SeokHun <tommya98@naver.com>
2024-08-10 21:39:50 +0900

    캘린더 - 특정 달의 최대 일수 출력하기

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("두 수를 입력하세요.");
+        int[] monthArray = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        System.out.println("달을 입력하세요.");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println("두 수의 합은 " + (a + b) + "입니다.");
+        int month = scanner.nextInt();
+        System.out.println(month + "월은 " + monthArray[month - 1] + "일까지 있습니다.");
     }
 }
