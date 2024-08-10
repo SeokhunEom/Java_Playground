d7170cdd9cb60e65d0058c3b01fbd122a8b6a01b
SeokHun <tommya98@naver.com>
2024-08-10 13:18:09 +0900

    구구단 - 배열로 구구단 다시 구현

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("구구단 중 출력할 단은? : ");
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        int[] result = new int[9];
+        for (int i = 2; i < 10; i++) {
+            for (int j = 0; j < result.length; j++) {
+                result[j] = i * (j + 1);
+            }
 
-        if (number < 2) {
-            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
-        } else if (number > 9) {
-            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
-        } else {
-            System.out.println("number = " + number);
-
-            for (int i = 1; i < 10; i++) {
-                System.out.println(number * i);
+            System.out.println(i + "단");
+            for (int j = 0; j < result.length; j++) {
+                System.out.println(result[j]);
             }
         }
     }
