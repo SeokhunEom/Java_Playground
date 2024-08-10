a494cd3106099808b343179af6b583b532544e10
SeokHun <tommya98@naver.com>
2024-08-10 13:36:54 +0900

    구구단 - 추가 미션 및 마무리하기

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int j = 2; j < 10; j++) {
-            System.out.println(j + "단");
-            Gugudan.print(Gugudan.calculate(j));
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] splitedValue = input.split(",");
+        int first = Integer.parseInt(splitedValue[0]);
+        int second = Integer.parseInt(splitedValue[1]);
+
+        for(int i = first; i <= second; i++) {
+            System.out.println(i + "단");
+            Gugudan.print(Gugudan.calculate(i));
         }
     }
 }
