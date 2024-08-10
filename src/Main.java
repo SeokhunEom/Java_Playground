052f14d8936763a2414deede794055a83d4d9c12
SeokHun <tommya98@naver.com>
2024-08-10 21:44:27 +0900

    캘린더 - 반복 입력 가능하도록 변경

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,16 @@ public class Main {
     public static void main(String[] args) {
         int[] monthArray = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        System.out.println("달을 입력하세요.");
+        System.out.println("반복횟수를 입력하세요.");
         Scanner scanner = new Scanner(System.in);
-        int month = scanner.nextInt();
-        System.out.println(month + "월은 " + monthArray[month - 1] + "일까지 있습니다.");
+        int count = scanner.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("달을 입력하세요.");
+            int month = scanner.nextInt();
+            System.out.println(month + "월은 " + monthArray[month - 1] + "일까지 있습니다.");
+        }
+
+        scanner.close();
     }
 }
