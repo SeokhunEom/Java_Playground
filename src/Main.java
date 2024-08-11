c248a85fb5a6de7cbb8508085c4196bb5ce62958
SeokHun <tommya98@naver.com>
2024-08-11 23:10:05 +0900

    캘린더 - 진짜 달력 자동으로 찍기

--- src/Main.java
+++ src/Main.java
@@ -13,28 +13,20 @@ public class Main {
             System.out.println("년도을 입력하세요.");
             System.out.print("> ");
             int year = scanner.nextInt();
+            if (year == -1) {
+                System.out.println("종료합니다.");
+                break;
+            }
 
             System.out.println("월을 입력하세요.");
             System.out.print("> ");
             int month = scanner.nextInt();
-            if (month == -1) {
-                System.out.println("종료합니다.");
-                break;
-            }
             if (month > 12 || month < 1) {
                 System.out.println("1부터 12까지의 숫자를 입력해주세요.");
                 continue;
             }
 
-            System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
-            System.out.print("> ");
-            String day1 = scanner.next().toUpperCase();
-            if (!day1.equals("SU") && !day1.equals("MO") && !day1.equals("TU") && !day1.equals("WE") && !day1.equals("TH") && !day1.equals("FR") && !day1.equals("SA")) {
-                System.out.println("잘못된 요일을 입력하셨습니다.");
-                continue;
-            }
-
-            Calendar.drawCalendar(year, month, day1);
+            MyCalendar.drawCalendar(year, month);
         }
     }
 }
--- src/MyCalendar.java
+++ src/MyCalendar.java
@@ -1,15 +1,18 @@
-public class Calendar {
+import java.util.Calendar;
+
+public class MyCalendar {
 
     public static final int[] MONTH_ARRAY = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    public static void drawCalendar(int year, int month, String day1) {
+    public static void drawCalendar(int year, int month) {
         if (isLeapYear(year)) MONTH_ARRAY[1] = 29;
-        int day1Num = dayToNum(day1);
+        int day1Num = getDay1(year, month);
+        System.out.println(day1Num);
 
         System.out.println("    <<" + year + "년 " + month + "월>>");
         System.out.println("SU MO TU WE TH FR SA");
         System.out.println("-------------------");
-        System.out.printf("%" + (day1Num * 3) + "s", "");
+        if(day1Num != 0) System.out.printf("%" + (day1Num * 3) + "s", "");
         for (int i = 1; i <= MONTH_ARRAY[month - 1]; i++) {
             System.out.printf("%3d", i);
             if ((i + day1Num) % 7 == 0) {
@@ -34,5 +37,11 @@ public class Calendar {
             case "SA" -> 6;
             default -> -1;
         };
+    }
+
+    private static int getDay1(int year, int month) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, 1);
+        return calendar.get(Calendar.DAY_OF_WEEK) - 1;
     }
 }
